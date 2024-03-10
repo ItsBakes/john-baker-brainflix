b5b6aaefa5d72d217477e103581e8d37736b0092
John <jobak1220@gmail.com>
2024-03-10 12:45:52 -0500

    add comment form field, avatar, and button

--- src/Components/CommentForm/CommentForm.js
+++ src/Components/CommentForm/CommentForm.js
@@ -5,6 +5,16 @@ function CommentForm (selectedVid) {
 
     return (
         <>
+        <div className="CommentForm__header">3 Comments</div>
+        <section className="CommentForm__inputField">
+            <img className="CommentForm__avatar"></img>
+            <form className="CommentForm__form">
+                <label className="CommentForm__label">
+                    <textarea type="text" className="CommentForm__textarea" placeholder="Add a new comment"></textarea>
+                </label>
+                <button className="CommentForm__button">comment</button>
+            </form>
+        </section>
         </>
     )
 }
