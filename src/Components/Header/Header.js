5ad9d99bf2cc9c98dc92b718902903dde9df6f4a
John <jobak1220@gmail.com>
2024-03-10 15:57:20 -0500

    add styling for header for mobile. need icon in search bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import Logo from '../../assets/images/BrainFlix-logo.svg'
 import Avatar from '../../assets/images/Mohan-muruge.jpg'
 import './Header.scss'
+import Search from '../../assets/images/search.svg'
 
 function Header() {
 
@@ -8,10 +9,11 @@ function Header() {
         <header className='header'>
             <div className="header__logo-container"><img className='header__logo' src={Logo}></img></div>
             <section className='header__search-container'>
-                <div className='header__search'><i className='header__icon'></i><input className='header__input' type='search' placeholder='Search'></input>
+                <div className='header__search'><input className='header__input' type='search' placeholder='Search'></input>
                 </div>
+                <div><img src={Avatar} alt='avatar' className='header__avatar'></img></div> 
                 <div className='header__upload'> <button className='header__button'>upload</button></div>
-                <div> <img src={Avatar} alt='avatar' className='header__avatar'></img></div>   
+                  
                 
             </section>
         </header>
