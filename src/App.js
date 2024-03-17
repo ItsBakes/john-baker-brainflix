0f439c97ddb27b12e9bda4b60181101d374510f0
John <jobak1220@gmail.com>
2024-03-17 13:01:55 -0500

    add path and element to main video page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.scss';
-import VideoDetails from './data/video-details.json'
-import Videos from './data/videos.json';
-import Header from './Components/Header/Header';
-import CurrentVideo from './Components/CurrentVideo/CurrentVideo'
-import { useState } from 'react';
-import NextVideos from './Components/NextVideos/NextVideos'
-import Comments from './Components/Comment/Comments';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UploadPage from './Pages/UploadPage/UploadPage';
+import MainVideoPage from './Pages/MainVideoPage/MainVideoPage'
 // https://unit-3-project-api-0a5620414506.herokuapp.com/
 // "api_key": "ce976863-77d9-4921-9e41-14e53f8217da"
 
@@ -16,9 +11,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route /> {/* need path and element in each route*/}
-        <Route />
-        <Route />
+        <Route path='/' element={<MainVideoPage/>}/> {/* need path and element in each route*/}
+        <Route path='/video/:idFromParams' element={<MainVideoPage/>} />
+        <Route path='/upload' element={<UploadPage/>}/>
       </Routes>
     </BrowserRouter>
   ); 
